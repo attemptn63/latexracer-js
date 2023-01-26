30bb25d9788b787ba0537aade70429864c637cef
attemptn63 <93580500+attemptn63@users.noreply.github.com>
2023-01-26 19:09:28 +0800

    trying to use requirejs

--- sketch.js
+++ sketch.js
@@ -1,12 +1,11 @@
-var requirejs = require('requirejs');
-requirejs.config({
-    nodeRequire: require
-});
+var en1k = require([english_1k.json]);
+var en10k = require([english_10k.json]);
+var en25k = require([english_25k.json]);
+var en450k = require([english_450k.json]);
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   background(220);
-  text(s, 100,100);
 }
