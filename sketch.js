1e59a9f273bb8dea668256f42926038b2f131b73
attemptn63 <93580500+attemptn63@users.noreply.github.com>
2023-01-26 18:27:09 +0800

    word lists

--- sketch.js
+++ sketch.js
@@ -1,9 +1,12 @@
+var requirejs = require('requirejs');
+requirejs.config({
+    nodeRequire: require
+});
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   background(220);
-  fill("black");
-  ellipse(50,50,80,80);
+  text(s, 100,100);
 }
