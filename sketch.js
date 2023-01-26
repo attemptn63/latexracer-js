f73194549cb0f1855d21ac43aea8146c97cdf437
attemptn63 <93580500+attemptn63@users.noreply.github.com>
2023-01-26 20:27:21 +0800

    help me
    i got the json to work properly

--- sketch.js
+++ sketch.js
@@ -4,11 +4,20 @@ function preload(){
   en10k = loadJSON("scripts/english_10k.json")
   en25k = loadJSON("scripts/english_25k.json")
   en450k = loadJSON("scripts/english_450k.json")
+  latex = loadJSON("scripts/code_latex.json")
 }
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background("#1b1b1b"); 
+  let words = random(latex.words)
+  for(let i = 0; i < 7; i++){
+    words = words + " " + random(latex.words)
+  }
+  textSize(20);
+  fill(255);
+  text(words, 10, 10, 500)
 }
 
 function draw() {
-  background(220);
+
 }
