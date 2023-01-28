62bd936f8244cde7da8c89cdc5a35472cafa8643
attemptn63 <93580500+attemptn63@users.noreply.github.com>
2023-01-28 17:52:34 +0800

    beeg update
    added buttons that changes length of text

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-let en1k,en10k,en25k,en450k,correct,wrong;
+let en1k,en10k,en25k,en450k,correct,wrong,button50,button100,button120,words,wordcnt = 50;
 function preload(){
   en1k = loadJSON("scripts/english_1k.json")
   en10k = loadJSON("scripts/english_10k.json")
@@ -7,22 +7,55 @@ function preload(){
   latex = loadJSON("scripts/code_latex.json")
   correct = color("#d1d0c5")
   wrong = color("#ca4754")
+  button50 = new Clickable();
+  button100 = new Clickable();
+  button120 = new Clickable();
 }
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background("#323437"); 
-  write(120)
+  setwords(wordcnt);
+  setbutton(button50, width/16+35, height/8, "50")
+  setbutton(button100, width/16+35, height/8 + 52, "100")
+  setbutton(button120, width/16+35, height/8 + 104, "120")
 }
-function write(wordcnt = 50){
-  let words = random(latex.words)
-  for(let i = 0; i < wordcnt ; i++){
+function setwords(wordcntr){
+  words = random(latex.words)
+  for(let i = 0; i < wordcntr ; i++){
     words = words + " " + random(latex.words)
   }
+}
+function write(wordcntr){
+  background("#323437");
+  fill(correct);
+  textSize(width/height * 11);
+  text("Length of text: ", width/16, height/8 - 35);
+  button50.draw();
+  button100.draw();
+  button120.draw();
+  push();
   textSize(width/height * 11); 
   fill("#646669");
   textFont("Inconsolata")
-  text(words, width/4 , height/(wordcnt * wordcnt * -0.000285714 + wordcnt * 0.112857 -1.42857), width/2)
+  text(words, width/4 , height/(wordcntr * wordcntr * -0.000285714 + wordcntr * 0.112857 -1.42857) - 35, width/2)
+  pop();
+}
+function setbutton(button, x, y, text){
+  button.locate(x, y);
+  button.resize(50, 50);
+  button.color = "#323437";
+  button.textColor = correct
+  button.stroke = "#323437";
+  button.text = text;
+  button.onPress = function(){
+    button50.stroke = "#323437";
+    button100.stroke = "#323437";
+    button120.stroke = "#323437";
+    button.stroke = "#d5ad16";
+    wordcnt = int(text);
+    setwords(wordcnt);
+  }
 }
 function draw() {
-
+  write(wordcnt);
 }
